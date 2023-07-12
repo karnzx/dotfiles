58c72d5ed3aa0df6a34f506be023d441b9e7ded8
Piravit <karnzx-18762@hotmail.com>
2023-07-12 23:16:13 +0700

    update script

--- run_once_after_1_edit_chezmoi_remote_url.sh
+++ run_once_after_1_edit_chezmoi_remote_url.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 function getGitRemoteURL(){
-	git remote get-url origin
+	chezmoi git remote get-url origin
 }
 currentRemoteURL=$(getGitRemoteURL)
 echo current git remote origin $currentRemoteURL
@@ -13,8 +13,8 @@ then
 	echo "already SSH | exit"
 	exit 0
 fi
-remoteSSH=$(git remote get-url origin | sed 's#https://github.com/#git@github.com:#')
+remoteSSH=$(chezmoi git remote get-url origin | sed 's#https://github.com/#git@github.com:#')
 echo change to $remoteSSH
-git remote set-url origin $remoteSSH
+chezmoi git remote set-url origin $remoteSSH
 
 echo ">> $(getGitRemoteURL)"
