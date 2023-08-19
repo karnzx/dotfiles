9383d458d6a3e4e425e5870715ae8c1acd342849
Piravit Chenpittaya <piravit.chenpittaya@gmail.com>
2023-08-19 13:04:27 +0700

    update change git remote url script

--- .chezmoiscripts/run_once_after_1_edit_chezmoi_remote_url.sh
+++ .chezmoiscripts/run_once_after_1_edit_chezmoi_remote_url.sh
@@ -13,8 +13,12 @@ then
 	echo "already SSH | exit"
 	exit 0
 fi
-remoteSSH=$(chezmoi git remote get-url origin | sed 's#https://github.com/#git@github.com:#')
-echo change to $remoteSSH
-chezmoi git remote set-url origin $remoteSSH
 
-echo ">> $(getGitRemoteURL)"
+read -p "Do you want to change git http url to ssh url? (y/n or enter): " -n 1
+if [[ "$REPLY" == "y" ]]; then
+    remoteSSH=$(chezmoi git remote get-url origin | sed 's#https://github.com/#git@github.com:#')
+    echo change to $remoteSSH
+    chezmoi git remote set-url origin $remoteSSH
+
+    echo ">> $(getGitRemoteURL)"
+fi
