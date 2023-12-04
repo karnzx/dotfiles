9fca52acd5450f3b7477e6df64257ae5862cbeef
Piravit <piravit.chenpittaya@gmail.com>
2023-12-04 20:54:08 +0700

    format file

--- .chezmoiscripts/run_once_after_1_edit_chezmoi_remote_url.sh
+++ .chezmoiscripts/run_once_after_1_edit_chezmoi_remote_url.sh
@@ -2,23 +2,22 @@
 
 set -o pipefail
 
-function getGitRemoteURL(){
-	chezmoi git remote get-url origin
+function getGitRemoteURL() {
+    chezmoi git remote get-url origin
 }
 currentRemoteURL=$(getGitRemoteURL)
-echo current git remote origin $currentRemoteURL
+echo current git remote origin "$currentRemoteURL"
 
-if [[ "$currentRemoteURL" == git@* ]]
-then
-	echo "already SSH | exit"
-	exit 0
+if [[ "$currentRemoteURL" == git@* ]]; then
+    echo "already SSH | exit"
+    exit 0
 fi
 
-read -p "Do you want to change git http url to ssh url? (y/n or enter): " -n 1
+read -r -p "Do you want to change git http url to ssh url? (y/n or enter): " -n 1
 if [[ "$REPLY" == "y" ]]; then
     remoteSSH=$(chezmoi git remote get-url origin | sed 's#https://github.com/#git@github.com:#')
-    echo change to $remoteSSH
-    chezmoi git remote set-url origin $remoteSSH
+    echo change to "$remoteSSH"
+    chezmoi git remote set-url origin "$remoteSSH"
 
     echo ">> $(getGitRemoteURL)"
 fi
