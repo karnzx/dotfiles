ccb97df215de93aa2d6836590891f0b17995a118
Piravit <piravit.chenpittaya@gmail.com>
2024-09-23 20:56:54 +0700

    feat add nvim config

---
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        dependencies = {
+          "arufars/snippet-next",
+        },
+      },
+    },
+  },
+}
